83c75d9a375389d426fdc091bc1f969a4e5c94e0
suketu-vaniya <150796982+SUKETU132@users.noreply.github.com>
2025-01-21 00:34:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   useEffect(() => {
     const getCurrentUser = async () => {
       try {
-        const response = await axios.get("https://expense-manager-nmegzi8kc-suketu-vaniyas-projects.vercel.app/api/v1/manager/user/getCurrentUser", {
+        const response = await axios.get("https://expense-manager-ten-gamma.vercel.app/api/v1/manager/user/getCurrentUser", {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
