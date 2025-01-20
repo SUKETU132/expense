e2185eefbf8f02a0c5d755a3776ed9c1034d7857
suketu-vaniya <150796982+SUKETU132@users.noreply.github.com>
2025-01-21 01:39:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   useEffect(() => {
     const getCurrentUser = async () => {
       try {
-        const response = await axios.get("expense-manager-three-eta.vercel.app/api/v1/manager/user/getCurrentUser", {
+        const response = await axios.get("https://expense-manager-5.onrender.com/api/v1/manager/user/getCurrentUser", {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
